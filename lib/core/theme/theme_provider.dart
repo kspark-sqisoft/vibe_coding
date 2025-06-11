841524d88158c5ee3f120be1933b435d499628ec
박기순 <ks.park@sqisoft.com>
2025-06-11 16:38:26 +0900

    테마 선택값 저장

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String _themeModeKey = 'themeMode';
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.light);
+  ThemeNotifier() : super(ThemeMode.light) {
+    _loadThemeMode();
+  }
+
+  Future<void> _loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt(_themeModeKey);
+    if (themeIndex != null) {
+      state = ThemeMode.values[themeIndex];
+    }
+  }
 
   void toggleTheme() {
     state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    _saveThemeMode(state);
+  }
+
+  Future<void> _saveThemeMode(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_themeModeKey, mode.index);
   }
 }
 
