83f8711ab7aab53d11c48d5e5ae431129ab9e8a1
박기순 <ks.park@sqisoft.com>
2025-06-11 14:43:09 +0900

    auth 모듈 제거

--- lib/features/auth/domain/auth_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'user_entity.dart';
-
-abstract class AuthRepository {
-  Future<UserEntity> signUp(String email, String password);
-  Future<UserEntity> signIn(String email, String password);
-  Future<void> signOut();
-}
--- lib/features/auth/domain/user_entity.dart
+++
@@ -1,6 +0,0 @@
-class UserEntity {
-  final String id;
-  final String email;
-
-  UserEntity({required this.id, required this.email});
-}
